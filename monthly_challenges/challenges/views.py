4ca5c0c5c22edb6a01cdd1a3ada3589199a6f531
Liddeldiiz <s218711@student.uek.krakow.pl>
2024-09-04 10:34:00 +0200

    Section 4 Complete

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,34 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat no meat for the whole month",
     "february": "Walk for at least 20 min every day",
     "march": "Learn Django for at least 20 min every day",
-    "april": "Eat no meat for the whole month",
-    "may": "Eat no meat for the whole month",
-    "june": "Eat no meat for the whole month",
-    "july": "Eat no meat for the whole month",
-    "august": "Eat no meat for the whole month",
-    "september": "Eat no meat for the whole month",
-    "october": "Eat no meat for the whole month",
-    "november": "Eat no meat for the whole month",
-    "december": "Eat no meat for the whole month"
+    "april": "Eat no vegtables for the whole month",
+    "may": "Run for at least 20 min every day",
+    "june": "Learn Python for at least 20 min every day",
+    "july": "Eat no chocolate for the whole month",
+    "august": "Hike for at least 40 min every day",
+    "september": "Learn Data Science for at least 20 min every day",
+    "october": "Eat no Fast Food for the whole month",
+    "november": "Drive on a Bike for at least 40 min every day",
+    "december": None
 }
+
+# Learn OSINT for at least 20 min every day
 
 # Create your views here.
 
-def challenges(reqeuest):
-    list_elements = ''
-    for month in list(monthly_challenges.keys()):
+def index(reqeuest):
+    months = list(monthly_challenges.keys())
 
-        #redirect_month = months[month-1]
-        redirect_path = reverse("month-challenge", args=[month])
-        print(redirect_path)
-        list_elements += f'<li><a href={redirect_path}>{month}</a></li>'
-    response_data = f'<ul>{list_elements}</ul>'
-    return HttpResponse(response_data)
+    return render(reqeuest, "challenges/index.html", {
+        "months": months
+    })
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
@@ -42,8 +41,11 @@ def monthly_challenge_by_number(request, month):
 
 def  monthly_challenge(request, month):
     try:
+        
         challenge_text = monthly_challenges[month]
-        response_data = f'<h1>{challenge_text}</h1>'
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "month": month,
+            "text": challenge_text
+        })
     except:
-        HttpResponseNotFound("<h1>This Month is not supported!</h1>")
+        raise Http404()
