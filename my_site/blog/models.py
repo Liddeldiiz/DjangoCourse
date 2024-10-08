124792e76cebbe897043ee22fd982384df685746
Liddeldiiz <s218711@student.uek.krakow.pl>
2024-10-08 12:38:40 +0200

    Section 9 completed

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.CharField(max_length=200)
+    date = models.DateField(blank=True, null=True)
+    date_modified = models.DateField(auto_now=True)
+
+    def get_Fullname(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.get_Fullname()
+    
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+    date = models.DateField(blank=True, null=True)
+    date_modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.caption
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.TextField()
+    image_name = models.CharField(max_length=80)
+    date = models.DateField()
+    date_modified = models.DateField(auto_now=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    content = models.TextField(default="", null=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
+    #author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    caption = models.ManyToManyField(Tag)
+
+    def get_absolute_url(self):
+        return reverse("post-detail", args=[self.slug])
+    
+
+    def save(self,*args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,106 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from datetime import date
+from .models import Post
 
 # Create your views here.
-
-posts_content = {
-    "First Post Ever": {
-        "Date": "05.09.2024",
-        "Content": "This is a simple Post. I think it would look kind of similar in a normal DB."
-    },
-    "Getting better at composing": {
-        "Date": "07.09.2024",
-        "Content": "You are asking yourself how to get better at composing music. Well this is easier than you think. You just have to let your mind go. Let your soul play the instrument. Thats when magic happens :D"
-    }
-}
-
-all_posts = [
-    {
-        "slug": "photography",
-        "image": "IMG_5461.JPG",
-        "author": "Liddeldiiz",
-        "date": date(2024, 10, 2),
-        "title": "Photography",
-        "excerpt": "Photography is the art of capturing a snapshot of a view or emotion",
-        "content": """
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum. 
-            """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "IMG_5259.JPG",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
 
 def get_date(post):
     return post['date']
 
 def index(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    posts = Post.objects.all()
+
     return render(request, 'blog/index.html', {
-        "posts": latest_posts
+        "posts": posts
     })
 
 def posts(request):
     #post_keys = list(posts_content.keys())
-
+    posts = Post.objects.all()
     return render(request, 'blog/all-posts.html', {
-        "posts": all_posts
-    })
-
-def post_blog(request, post_key):
-    post_content = posts_content[post_key]
-    return render(request, "blog/post_blog.html", {
-        'titleOfPost': post_key,
-        'content': post_content['Content'],
-        'date': post_content['Date']
+        "posts": posts
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    post = get_object_or_404(Post, slug=slug)
+    #identified_post = next(post for post in all_posts if post['slug'] == slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": post
     })
